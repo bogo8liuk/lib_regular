44457aac3022e2ee115c0fce0459fcd8aca5fc5c
bogo8liuk <lucaborghi99@gmail.com>
2021-03-28 12:59:19 +0200

    interface improved

--- regular.mli
+++ regular.mli
@@ -1,34 +1,47 @@
 type regdef
 
-val create_regdef : string -> regdef
+module type reg =
+    sig
+        type definition
+
+        val create : string -> definition
+
+        exception Invalid_definition of string
+    end
+;;
 
 (* <function_name>_raw param_1 ... ~regdef:<string_1> ... param_n
      is equivalent to call:
-   <function_name> param_1 ... ~regdef:(create_regdef <string_1>) ... param_n *)
+   <function_name> param_1 ... ~regdef:(reg.create <string_1>) ... param_n *)
 
-(* It defines and registers a rule. A rule is a regular definition that can be used through its name. *)
-val create_rule : ~name:string -> ~regdef:regdef -> unit
-val create_rule_raw : ~name:string -> ~regdef:string -> unit
+module type rule =
+    sig
+        type name = string
 
-val rule_to_regdef : ~rule_name:string -> regdef
+        (* It defines and registers a rule. A rule is a regular definition that can be used through its name. *)
+        val create : ~name:name -> ~regdef:reg.definition -> unit
+        val create_raw : ~name:name -> ~regdef:string -> unit
+
+        val to_regdef : ~name:name -> reg.definition
+
+        exception Inexistent_rule of name
+    end
+;;
 
 (* <function_name>_rule param_1 ... ~rule_name:<string_1> ... param_n
      is equivalent to call:
-   <function_name> param_1 ... ~regdef:(rule_to_regdef <string_1>) ... param_n *)
+   <function_name> param_1 ... ~regdef:(rule.to_regdef <string_1>) ... param_n *)
 
 (* It returns true if the string 'to_match' matches exactly with 'regdef'. *)
-val is_matching : ~to_match:string -> ~regdef:regdef -> bool
+val is_matching : ~to_match:string -> ~regdef:reg.definition -> bool
 val is_matching_raw : ~to_match:string -> ~regdef:string -> bool
-val is_matching_rule : ~to_match:string -> ~rule_name:string -> bool
+val is_matching_rule : ~to_match:string -> ~name:rule.name -> bool
 
-val is_matching_prefix : ~to_match:string -> ~regdef:regdef -> bool
+val is_matching_prefix : ~to_match:string -> ~regdef:reg.definition -> bool
 val is_matching_prefix_raw : ~to_match:string -> ~regdef:string -> bool
-val is_matching_prefix_rule : ~to_match:string -> ~rule_name:string -> bool
+val is_matching_prefix_rule : ~to_match:string -> ~name:rule.name -> bool
 
-val is_matching_suffix : ~to_match:string -> ~regdef:regdef -> bool
+val is_matching_suffix : ~to_match:string -> ~regdef:reg.definition -> bool
 val is_matching_suffix_raw : ~to_match:string -> ~regdef:string -> bool
-val is_matching_suffix_rule : ~to_match:string -> ~rule_name:string -> bool
-
-exception Invalid_regdef of string
-exception Inexistent_rule of string
+val is_matching_suffix_rule : ~to_match:string -> ~name:rule.name -> bool
 ;;
--- string.mli
+++ string.mli
@@ -1,34 +1,34 @@
-val match_count : ~to_check:string -> ~regdef:regdef -> int
+val match_count : ~to_check:string -> ~regdef:reg.definition -> int
 val match_count_raw : ~to_check:string -> ~regdef:string -> int
-val match_count_rule : ~to_check:string -> ~rule_name:string -> int
+val match_count_rule : ~to_check:string -> ~name:rule.name -> int
 
-val matching_substrings : ~to_check:string -> ~regdef:regdef -> string list
+val matching_substrings : ~to_check:string -> ~regdef:reg.definition -> string list
 val matching_substrings_raw : ~to_check:string -> ~regdef:string -> string list
-val matching_substrings_rule : ~to_check:string -> ~rule_name:string -> string list
+val matching_substrings_rule : ~to_check:string -> ~name:rule.name -> string list
 
-val split_string : ~to_change:string -> ~regdef:regdef -> string list
+val split_string : ~to_change:string -> ~regdef:reg.definition -> string list
 val split_string_raw : ~to_change:string -> ~regdef:string -> string list
-val split_string_rule : ~to_change:string -> ~rule_name:string -> string list
+val split_string_rule : ~to_change:string -> ~name:rule.name -> string list
 
 (* It substitutes every substring in 'to_change' matching with 'regdef' with the result of the call:
      substitution to_compute
    The function is granted to be called at every match and not once for all. *)
-val replace_with : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~regdef:regdef -> string
-val replace_with_raw : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~regdef:string -> string
-val replace_with_rule : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~rule_name:string -> string
+val replace_with : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~regdef:reg.definition -> string
+val replace_with_raw : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~regdef:string -> string
+val replace_with_rule : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~name:rule.name -> string
 
 (* It substitutes every substring in 'to_change' matching with 'regdef' with 'to_substitute' string. *)
-val replace_string : ~to_change:string -> ~to_substitute:string -> ~regdef:regdef -> string
+val replace_string : ~to_change:string -> ~to_substitute:string -> ~regdef:reg.definition -> string
 val replace_string_raw : ~to_change:string -> ~to_substitute:string -> ~regdef:string -> string
-val replace_string_rule : ~to_change:string -> ~to_substitute:string -> ~rule_name:string -> string
+val replace_string_rule : ~to_change:string -> ~to_substitute:string -> ~name:rule.name -> string
 
 (* It is same as 'replace_with', except that it replaces the first 'times' matches. If there is less
    matches than 'times', this call is equivalent to 'replace_with'. *)
-val replace_with_first : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~regdef:regdef -> ~times:int -> string
-val replace_with_first_raw : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~regdef:string -> ~times:int -> string
-val replace_with_first_rule : ~to_change:string -> ~substitution:(string -> string) -> ~to_compute:string -> ~rule_name:string -> ~times:int -> string
+val replace_with_first : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~regdef:reg.definition -> ~times:int -> string
+val replace_with_first_raw : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~regdef:string -> ~times:int -> string
+val replace_with_first_rule : ~to_change:string -> ~substitution:('a -> string) -> ~to_compute:'a -> ~name:rule.name -> ~times:int -> string
 
-val replace_string_first : ~to_change:string -> ~to_substitute:string -> ~regdef:regdef -> ~times:int -> string
-val replace_string_first_raw : ~to_change:string -> ~to_substitute:string -> ~regdef:regdef -> ~times:int -> string
-val replace_string_first_rule : ~to_change:string -> ~to_substitute:string -> ~regdef:regdef -> ~times:int -> string
+val replace_string_first : ~to_change:string -> ~to_substitute:string -> ~regdef:reg.definition -> ~times:int -> string
+val replace_string_first_raw : ~to_change:string -> ~to_substitute:string -> ~regdef:string -> ~times:int -> string
+val replace_string_first_rule : ~to_change:string -> ~to_substitute:string -> ~name:rule.name -> ~times:int -> string
 ;;
